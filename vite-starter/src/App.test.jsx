a3072647458a8dc4a73b47ee31ded9ca3ac2e944
asilva-dev <amanda.adgti@gmail.com>
2024-04-08 22:34:49 -0300

    starting the repository

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,24 @@
 import { render, screen } from "@testing-library/react";
+
 import App from "./App";
 
-test("App contains correct heading", () => {
+test("Button starts with correct color", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", {name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
+});
+
+test("Button starts with correct text", () => {
+  render(<App />);
+  
+});
+
+test("Button has correct color after click", () => {
+  render(<App />);
+  
+});
+
+test("Button has correct text after click", () => {
+  render(<App />);
+  
 });
